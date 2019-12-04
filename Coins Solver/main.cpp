563d2e1c603232586759e1ac21b6e8597af4da05
George <georgii.kashin@gmail.com>
2019-12-04 16:01:57 +0300

    Initial Commit

---
+++ Coins Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coins Solver
+//
+//  Created by Георгий Кашин on 04.12.2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
